720e4315ab5902ca9557f79ad5374b56e6eddadb
The Stretch <ats92@cox.net>
2021-10-17 22:20:13 -0700

    added post for new note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
-const  note  = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
+const note  = require('../../db/db.json');
 
+
+function createNote(body, note) {
+    const notes = body;
+    note.push(notes);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(note, null, 2)
+    );
+    
+};
 router.get('/notes', (req, res) => {
-    console.log(note);
     res.json(note);
 });
 
 
 router.post('/notes', (req, res) =>{
-    res.json(req.body);
+    req.body.id = note.length.toString();
+    const notes = createNote(req.body, note);
+    res.json(notes);
 })
 
 module.exports = router;
